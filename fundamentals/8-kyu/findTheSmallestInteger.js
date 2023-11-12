844cf93c33284c89878c98810baabe284610ff93
Hisban Shiraz <hisbanbardai@gmail.com>
2023-11-12 08:53:31 -0700

    Implemented codewars challenge Find the smallest integer in the array

---
+++ fundamentals/8-kyu/findTheSmallestInteger.js
@@ -0,0 +1,26 @@
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    let smallest = args[0];
+    args.forEach(element => {
+      if (element < smallest) {
+        smallest = element;
+      }
+    });
+    return smallest;
+  }
+}
+
+//solution I liked
+// class SmallestIntegerFinder {
+//   findSmallestInt(args) {
+//     return Math.min(...args);
+//   }
+// }
