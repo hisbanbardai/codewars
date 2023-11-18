14f029e8b6a4f8d734f225d1f8f01cab3a876951
hisbanshiraz <hisbanbardai@gmail.com>
2023-11-18 16:06:42 -0700

    Fixes in code

--- fundamentals/8-kyu/calculateBMI.js
+++ fundamentals/8-kyu/calculateBMI.js
@@ -8,7 +8,7 @@
 
 // if bmi > 30 return "Obese"
 
-function bmi(weight, height) {
+function bmi(weight, height) { 
   const bmi = weight / (height * height);
 
   if (bmi <= 18.5) return "Underweight";
@@ -18,7 +18,7 @@ function bmi(weight, height) {
   if (bmi <= 30.0) return "Overweight";
 
   if (bmi > 30) return "Obese";
-}
+} 
 
 
 //solution I liked
